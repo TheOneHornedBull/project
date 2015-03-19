e7079f11930a9c3c88b1c60890faf97efc967fde
Kris Vrajilov <pimpspride@gmail.com>
2015-03-19 13:06:52 +0200

    почти готово, трябва да се оправят playerBullet

---
+++ Assets/Scripts/playerBulletScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerBulletScript : MonoBehaviour {
+	void Start () {
+		GetComponent<Rigidbody>().velocity = transform.up * 30;
+		Destroy (gameObject,3);
+	}
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "boss") {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/touchInput.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class touchInput : MonoBehaviour {
+
+	private Vector3 touchPoint;
+	private int HP = 120;
+	private float nextFire;
+	public GameObject playerBullet;
+	
+	void FixedUpdate () {
+
+		Shooting ();
+
+		if (HP <= 0) {
+			Debug.Log("Game over ! You have been defeated.");
+			Time.timeScale = 0;
+		}
+
+		Movement ();
+	}
+
+	void Movement () {
+
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch (0);
+
+
+
+			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved || Input.GetMouseButtonDown(0)) {
+				Ray ray;
+				RaycastHit hit;
+
+				#if UNITY_EDITOR
+				ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				
+				#elif (UNITY_ANDROID || UNITY_IPHONE || UNITY_WP8)
+				ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+				
+				#endif
+				
+				if(Physics.Raycast(ray,out hit)){
+					touchPoint = hit.point;
+				}
+			}
+			transform.position = Vector3.Lerp (transform.position, new Vector3 (touchPoint.x, transform.position.y, transform.position.z), Time.deltaTime * 100);
+		}
+	}
+
+	void Shooting () {
+		if (Time.time > nextFire) {
+			nextFire = Time.time + 0.2f;
+			Instantiate (playerBullet, transform.position + new Vector3 (3, 4, 0), transform.rotation);
+			Debug.Log("fire");
+		}
+	} 
+
+	void OnTriggerEnter (Collider other){
+		if (other.tag == "enemyBullet") {
+			HP -= 5;
+		}
+	}
+
+	void OnGUI () {
+		GUI.Label(new Rect(10, 40, 100, 40), HP.ToString());
+	}
+
+}
